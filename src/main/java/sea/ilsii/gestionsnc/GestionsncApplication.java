bf6f20967ab720e80af171b532cc0e789c4c181f
lafleure <azizgouem29@gmail.com>
2023-07-05 23:40:20 +0100

    premier commit

---
+++ src/main/java/sea/ilsii/gestionsnc/GestionsncApplication.java
@@ -0,0 +1,13 @@
+package sea.ilsii.gestionsnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionsncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionsncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sea/ilsii/gestionsnc/GestionsncApplicationTests.java
@@ -0,0 +1,13 @@
+package sea.ilsii.gestionsnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionsncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
